22a275b6b163dbc07e1c2c6a3beb45daac5d027c
Oussama Alouat <oussama.alouat@ciberexperis.es>
2023-03-21 15:49:49 +0100

    Added apllo

---
+++ cotown/src/app/graphql.module.ts
@@ -0,0 +1,50 @@
+import { Apollo, APOLLO_OPTIONS, ApolloModule } from 'apollo-angular';
+import { HttpLink } from 'apollo-angular/http';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { ApolloLink, InMemoryCache } from '@apollo/client/core';
+import { setContext } from '@apollo/client/link/context';
+
+const uri = 'https://experis.flows.ninja'; // <-- add the URL of the GraphQL server here
+ 
+export function createApollo(httpLink: HttpLink) {
+  const basic = setContext((operation, context) => ({
+    headers: {
+      Accept: 'charset=utf-8',
+    },
+  }));
+ 
+  const auth = setContext((operation, context) => {
+    const token = localStorage.getItem('token');
+ 
+    if (token === null) {
+      return {};
+    } else {
+      return {
+        headers: {
+          Authorization: `${token}`,
+        },
+      };
+    }
+  });
+ 
+  const link = ApolloLink.from([basic, auth, httpLink.create({ uri })]);
+  const cache = new InMemoryCache();
+ 
+  return {
+    link,
+    cache,
+  };
+}
+ 
+@NgModule({
+  exports: [HttpClientModule, ApolloModule],
+  providers: [
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: createApollo,
+      deps: [HttpLink],
+    },
+  ],
+})
+export class GraphQLModule {}

67703b78357601aba6b8b4e86dfd1bd16d29993c
Alejandro André <alejandro.andre@ciberexperis.es>
2023-03-28 11:54:20 +0200

    Fix

--- cotown/src/app/app.module.ts
+++ cotown/src/app/app.module.ts
@@ -2,26 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-
-import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SecondComponent } from './pages/second/second.component';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
+import { PlanningComponent } from './pages/planning/planning.component';
+import { TimeChartModule } from './time-chart/time-chart.module';
+import { CotownModal } from './pages/cotown-modal/cotown-modal.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    SecondComponent
+    SecondComponent,
+    PlanningComponent,
+    CotownModal
   ],
   imports: [
     BrowserModule,
@@ -33,7 +36,8 @@ import { HttpClientModule } from '@angular/common/http';
     MatFormFieldModule,
     MatSelectModule,
     GraphQLModule,
-    HttpClientModule
+    HttpClientModule,
+    TimeChartModule
   ],
   bootstrap: [AppComponent]
 })

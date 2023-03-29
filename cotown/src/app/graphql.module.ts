d2c779f44af0c1266a65244a1dd7a006d2e82f8d
Oussama Alouat <oussama.alouat@ciberexperis.es>
2023-03-29 10:26:02 +0200

    FIX: remove blank lines

--- cotown/src/app/graphql.module.ts
+++ cotown/src/app/graphql.module.ts
@@ -1,4 +1,4 @@
-import { Apollo, APOLLO_OPTIONS, ApolloModule } from 'apollo-angular';
+import { APOLLO_OPTIONS, ApolloModule } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
@@ -6,24 +6,23 @@ import { ApolloLink, InMemoryCache } from '@apollo/client/core';
 import { setContext } from '@apollo/client/link/context';
 
 const uri = 'https://experis.flows.ninja/graphql'; // <-- add the URL of the GraphQL server here
- 
+
 export function createApollo(httpLink: HttpLink) {
   const basic = setContext((operation, context) => ({
     headers: {
       Accept: 'charset=utf-8',
     },
   }));
- 
- 
+
   const link = ApolloLink.from([basic, httpLink.create({ uri })]);
   const cache = new InMemoryCache();
- 
+
   return {
     link,
     cache,
   };
 }
- 
+
 @NgModule({
   exports: [HttpClientModule, ApolloModule],
   providers: [

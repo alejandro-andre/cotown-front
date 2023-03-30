22a24ab52c4dcf5bf95945bfe6d19af726ffbfba
Oussama Alouat <oussama.alouat@ciberexperis.es>
2023-03-30 15:43:44 +0200

    FEAT: set new datePciker - Fix load error

--- cotown/src/app/app.module.ts
+++ cotown/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SecondComponent } from './pages/second/second.component';
@@ -33,6 +35,8 @@ import { CotownModal } from './pages/cotown-modal/cotown-modal.component';
     BrowserAnimationsModule,
     MatButtonModule,
     MatTableModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
     MatFormFieldModule,
     MatSelectModule,
     GraphQLModule,

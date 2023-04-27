f61d80021198fe964b06c8be2af22eccc2d6f8fd
Oussama Alouat <oussama.alouat@ciberexperis.es>
2023-04-27 11:23:19 +0200

    FIX: fixed imports and divided them

--- customer/src/app/customer/customer.module.ts
+++ customer/src/app/customer/customer.module.ts
@@ -1,28 +1,36 @@
+// Core
 import { NgModule } from '@angular/core';
-
-import { CustomerRoutingModule } from './customer-routing.module';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { MatFormFieldModule,  } from '@angular/material/form-field';
-import { MatInputModule  } from '@angular/material/input';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+// Material
 import { MatSelectModule } from '@angular/material/select'
 import { MatDatepickerModule } from '@angular/material/datepicker'
 import { MatNativeDateModule } from '@angular/material/core';
+import { MatFormFieldModule,  } from '@angular/material/form-field';
+import { MatInputModule  } from '@angular/material/input';
 
-
+// Components
 import { HomeComponent } from './home/home.component';
 import { LayoutComponent } from './layout/layout.component';
 import { NavComponent } from './nav/nav.component';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { MyDataComponent } from './myData/myData.component';
+import { MyContactsComponent } from './myContacts/myContacts.component';
+import { NewContactComponent } from './myContacts/newContacts/newContact.component';
+
+// Routing
+import { CustomerRoutingModule } from './customer-routing.module';
 
 @NgModule({
   declarations: [
     HomeComponent,
     NavComponent,
     LayoutComponent,
-    MyDataComponent
+    MyDataComponent,
+    MyContactsComponent,
+    NewContactComponent
   ],
   imports: [
     CommonModule,

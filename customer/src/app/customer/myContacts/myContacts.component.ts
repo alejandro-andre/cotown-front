378a5c4b8baeaa5bcdd53c0a9600311365997f13
Oussama Alouat <oussama.alouat@ciberexperis.es>
2023-04-27 09:32:12 +0200

    FEAT: added myContacts component

---
+++ customer/src/app/customer/myContacts/myContacts.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './myContacts.component.html',
+  styleUrls: ['./myContacts.component.scss']
+})
+
+export class MyContactsComponent implements OnInit{
+
+  constructor() {}
+  ngOnInit(): void {
+  }
+}

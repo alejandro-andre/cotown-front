67ceb0f8bace85ec961dd8d5a3ed1df8264473fd
Oussama Alouat <oussama.alouat@ciberexperis.es>
2023-05-05 09:14:48 +0200

    FEAT: add some constabts

--- customer/src/app/constants/Constants.ts
+++ customer/src/app/constants/Constants.ts
@@ -26,21 +26,44 @@ export class Constants {
     url: 'invoices'
   };
 
+  // GENERALS
+  public static DATE = 'date';
+  public static RESOURCE = 'resource';
+  public static CODE = 'code';
+  public static CONCEPT = 'concept'
+
   // Table header constants of myBookingDetail components
-  public static BOOKING_DETAIL_DATE_HEADER = 'date';
+  public static BOOKING_DETAIL_DATE_HEADER = this.DATE;
   public static BOOKING_DETAIL_RENT_HEADER = 'rent';
   public static BOOKING_DETAIL_SERVICE_HEADER = 'service';
   public static BOOKING_DETAIL_RENT_DISCOUN_HEADER = 'rent_discount';
   public static BOOKING_DETAIL_SERVICE_DISCOUNT_HEADER = 'service_discount';
 
   // Table header constants of myBooking component
-  public static BOOKING_RESOURCE = 'resource';
+  public static BOOKING_RESOURCE = this.RESOURCE;
   public static BOOKING_FROM = 'from';
   public static BOOKING_TO = 'to';
   public static BOOKING_STATUS = 'status';
   public static BOOKING_FLAT = 'flat';
   public static BOOKING_PLACE = 'place';
   public static BOOKING_RESOURCE_TYPE = 'resourceType';
+
+  // Table header constants of Invoices
+  public static INVOICE_CONCEPT = this.CONCEPT;
+  public static INVOICE_RESOURCE= this.RESOURCE;
+  public static INVOICE_TOTAL = 'total';
+  public static INVOICE_DATE = this.DATE;
+  public static INVOICE_CODE = this.CODE;
+
+
+  // Table header constants of Payment
+  public static PAYMENT_CONCEPT = this.CONCEPT;
+  public static PAYMENT_RESOURCE = this.RESOURCE;
+  public static PAYMENT_CODE = this.CODE;
+  public static PAYMENT_AMOUNT = 'amount';
+  public static PAYMENT_PAY = 'pay';
+  public static PAYMENT_DATE = this.DATE;
+
 
   /**
    * Urls of nav

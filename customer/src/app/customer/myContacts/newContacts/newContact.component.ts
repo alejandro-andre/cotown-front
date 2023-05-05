03fcdb4138346f9c7a8f4e1157b49c1a260c6920
Oussama Alouat <oussama.alouat@ciberexperis.es>
2023-05-05 14:42:15 +0200

    FIX: saved new contact from the fronted

--- customer/src/app/customer/myContacts/newContacts/newContact.component.ts
+++ customer/src/app/customer/myContacts/newContacts/newContact.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { BasicResponse } from 'src/app/constants/Interface';
+import { SET_NEW_CONTACT } from 'src/app/schemas/query-definitions/contact.query';
+import { ApoloQueryApi } from 'src/app/services/apolo-api.service';
 import { ContactTypeService } from 'src/app/services/contactType.service';
 import { CustomerService } from 'src/app/services/customer.service';
 
@@ -14,6 +16,7 @@ export class NewContactComponent {
   constructor(
     public customerService: CustomerService,
     private contactTypeService: ContactTypeService,
+    private apollo: ApoloQueryApi,
     private router: Router,
   ) {}
 
@@ -29,5 +32,37 @@ export class NewContactComponent {
 
   changed() {
     console.log(' HERE AT THE CONTACT')
+  }
+
+  get isDisabled (): boolean {
+    return this.contactType === null || this.name === '' || (this.email === '' && this.phone === '');
+  }
+
+  save() {
+    const variables:{
+      id: number,
+      cid: number | null,
+      name: string,
+      email: string | undefined,
+      phone: string | undefined
+    } = {
+      id: 916,
+      cid: this.contactType,
+      name: this.name,
+      email: this.email,
+      phone: this.phone
+    };
+
+    if(this.email === '') {
+      delete variables.email
+    }
+
+    if(this.phone === '') {
+      delete variables.phone
+    }
+
+    this.apollo.setData(SET_NEW_CONTACT, variables).subscribe((ev) => {
+      console.log('subscription on apollo: ', ev);
+    })
   }
 }

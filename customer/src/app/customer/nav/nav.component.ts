fcf8f1020764ff5d3977f276029f9b86d0726f71
Oussama Alouat <oussama.alouat@ciberexperis.es>
2023-05-08 16:56:57 +0200

    FEAT: added active route

--- customer/src/app/customer/nav/nav.component.ts
+++ customer/src/app/customer/nav/nav.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Constants } from 'src/app/constants/Constants';
 
 @Component({
@@ -7,13 +8,33 @@ import { Constants } from 'src/app/constants/Constants';
   styleUrls: ['./nav.component.scss']
 })
 
-export class NavComponent {
+export class NavComponent implements OnInit{
   @Output() onSelectOption: EventEmitter<string> = new EventEmitter();
   public navs = Constants.NAV_URLS;
+  public selected: string = '';
 
-  constructor() { }
+  constructor(
+    private _router: Router
+  ) {}
+  ngOnInit(): void {
+
+  }
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.setActiveRoute();
+    }, 300);
+  }
+
+  setActiveRoute(): void {
+    const url = this._router.url ? this._router.url : '';
+    if (this._router.url && this._router.url !== '') {
+      const arrayOfSplitedUrls = url.split('/');
+      this.selected = arrayOfSplitedUrls[arrayOfSplitedUrls.length -1 ];
+    }
+  }
 
   onSelect(data: string) {
+    this.selected = data;
     this.onSelectOption.next(data);
   }
 }

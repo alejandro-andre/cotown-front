e04f178c368881da733fc55ea1c3a6244b31467f
Oussama Alouat <oussama.alouat@ciberexperis.es>
2023-05-11 13:42:08 +0200

    FIX: use only one input to set name & surename

--- customer/src/app/customer/myContacts/newContacts/newContact.component.ts
+++ customer/src/app/customer/myContacts/newContacts/newContact.component.ts
@@ -22,7 +22,6 @@ import { GET_CONTACTS_BY_CUSTOMERID, SET_NEW_CONTACT } from 'src/app/schemas/que
 export class NewContactComponent {
   public contactType!: number;
   public name: string = '';
-  public surName: string = '';
   public email: string = '';
   public phone: string = '';
   public emailFormControl = new FormControl('', [
@@ -46,9 +45,8 @@ export class NewContactComponent {
   }
 
   save() {
-    const name = this.surName.length > 0 ? `${this.name} ${this.surName}` : this.name;
     const variables: ContactVariables = {
-      name,
+      name: this.name,
       id: this.customerService.customer.id,
       cid: this.contactType,
       email: this.email,

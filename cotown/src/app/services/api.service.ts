cc89c23fec64ea9284c441ad260b22e349839cc6
Alejandro André <alejandro.andre@ciberexperis.es>
2023-05-20 14:37:07 +0200

    access_token in querystring

--- cotown/src/app/services/api.service.ts
+++ cotown/src/app/services/api.service.ts
@@ -5,19 +5,19 @@ import { AvailabilityPayload } from '../constants/Interfaces';
 export default {
 
   getAvailability(data: AvailabilityPayload, token: string): Promise<AxiosResponse> {
-    return axiosInstance.post('availability', data);
+    return axiosInstance.post('availability', data, { params: { 'access_token': token } } );
   },
 
   getDashboard(token: string): Promise<AxiosResponse> {
-    return axiosInstance.get('dashboard');
+    return axiosInstance.get('dashboard', { params: { 'access_token': token } } );
   },
 
   getDashboardBookings(status: string, token: string): Promise<AxiosResponse> {
-    return axiosInstance.get('dashboard/' + status);
+    return axiosInstance.get('dashboard/' + status, { params: { 'access_token': token } } );
   },
 
   getLabels(id: number, locale: string, token: string): Promise<AxiosResponse> {
-    return axiosInstance.get('labels/' + id + '/' + locale);
+    return axiosInstance.get('labels/' + id + '/' + locale, { params: { 'access_token': token } } );
   }
 
 }

09550df8539d186faf688c5a6a8f6ebdb4f76563
Oussama Alouat <oussama.alouat@ciberexperis.es>
2023-05-22 09:21:12 +0200

    FEAT: added payment Ok component

---
+++ customer/src/app/customer/paymentOk/paymentOk.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-contacts',
+  templateUrl: './paymentOk.component.html',
+  styleUrls: ['./paymentOk.component.scss']
+})
+
+export class PaymentOkComponent {
+  constructor(
+  ) {}
+
+}

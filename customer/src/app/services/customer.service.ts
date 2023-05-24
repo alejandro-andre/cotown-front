1dba68b01500eb7ba014edd22814c434792ba913
Oussama Alouat <oussama.alouat@ciberexperis.es>
2023-05-24 16:33:12 +0200

    FIX: fixed view section to update customer

--- customer/src/app/services/customer.service.ts
+++ customer/src/app/services/customer.service.ts
@@ -53,73 +53,107 @@ export class CustomerService {
     this.customer.contacts = copy.filter((ev: Contact) => ev.id !== id);
   }
 
-   private setVisibility(): void {
+  setVisibility(): void {
     if (this.customer.name === '' || this.customer.name === null) {
       this.readOnly.name = false;
+    } else {
+      this.readOnly.name = true;
     }
 
     if (this.customer.province === '' || this.customer.province === null) {
       this.readOnly.province = false;
+    } else {
+      this.readOnly.province = true;
     }
 
     if (this.customer.city === '' || this.customer.city === null) {
       this.readOnly.city = false;
+    } else {
+      this.readOnly.city = true;
     }
 
     if (this.customer.country === null) {
       this.readOnly.country = false;
+    } else {
+      this.readOnly.country = true;
     }
 
     if (this.customer.address === '' || this.customer.address === null) {
       this.readOnly.address = false;
+    } else {
+      this.readOnly.address = true;
     }
 
     if (this.customer.postalCode === '' || this.customer.postalCode === null) {
       this.readOnly.postalCode = false;
+    } else {
+      this.readOnly.postalCode = true;
     }
 
     if (this.customer.document === '' || this.customer.document === null) {
       this.readOnly.document = false;
+    } else {
+      this.readOnly.document = true;
     }
 
     if (this.customer.email === '' || this.customer.email === null) {
       this.readOnly.email = false;
+    } else {
+      this.readOnly.email = true;
     }
 
     if (this.customer.phone === '' || this.customer.phone === null) {
       this.readOnly.phone = false;
+    } else {
+      this.readOnly.phone = true;
     }
 
     if (this.customer.genderId === null) {
       this.readOnly.genderId = false;
+    } else {
+      this.readOnly.genderId = true;
     }
 
     if (this.customer.languageId === null) {
       this.readOnly.languageId = false;
+    } else {
+      this.readOnly.languageId = true;
     }
 
     if (this.customer.originId === null) {
       this.readOnly.originId = false;
+    } else {
+      this.readOnly.originId = true;
     }
 
     if (this.customer.nationality === null) {
       this.readOnly.nationality = false;
+    } else {
+      this.readOnly.nationality = true;
     }
 
     if (this.customer.birthDate === null) {
       this.readOnly.birthDate = false;
+    } else {
+      this.readOnly.birthDate = true;
     }
 
     if (this.customer.typeDoc === null) {
       this.readOnly.typeDoc = false;
+    } else {
+      this.readOnly.typeDoc = true;
     }
 
     if (this.customer.schoolOrCompany === null) {
       this.readOnly.schoolOrCompany = false;
+    } else {
+      this.readOnly.schoolOrCompany = true;
     }
 
     if (this.customer.bankAcount === '' || this.customer.bankAcount === null) {
       this.readOnly.bankAcount = false;
+    } else {
+      this.readOnly.bankAcount = true;
     }
   }
 }

2f33cbea43747a2780e2ef65cf4b66061219dad4
Alejandro André <alejandro.andre@ciberexperis.es>
2023-05-27 19:44:23 +0200

    Download

--- cotown/src/app/pages/download/download.component.ts
+++ cotown/src/app/pages/download/download.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
-import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { ApolloQueryApi as ApolloQueryApi } from 'src/app/services/apollo-api.service';
 
@@ -12,23 +10,14 @@ import { ApolloQueryApi as ApolloQueryApi } from 'src/app/services/apollo-api.se
 
 export class DownloadComponent {
 
-    public excelUrl: SafeUrl | undefined;
-
     // Constructor
     constructor(
-      private http: HttpClient,
       private apolloApi: ApolloQueryApi,
-      private sanitizer: DomSanitizer
     ) { }    
 
-    // Download excel
-    link(data: string) : boolean {
-      console.log(data)
-      const url = environment.backURL + '/export/' + data + '?access_token=' + this.apolloApi.token;
-      this.http.get(url, { responseType: 'blob' }).subscribe((blob: Blob) => {
-        this.excelUrl = this.sanitizer.bypassSecurityTrustUrl(window.URL.createObjectURL(blob));
-      });
-      return false;
+    // Link
+    link(data: string) : string {
+      return environment.backURL + '/export/' + data + '?access_token=' + this.apolloApi.token;
     }    
 }
 

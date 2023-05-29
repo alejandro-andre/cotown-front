1a79528ca7060bfa18e94463da5569ae6af1b068
Oussama Alouat <oussama.alouat@ciberexperis.es>
2023-05-29 16:04:53 +0200

    FIX: add name query of add new contact

--- customer/src/app/schemas/query-definitions/contact.query.ts
+++ customer/src/app/schemas/query-definitions/contact.query.ts
@@ -1,5 +1,5 @@
 export const SET_NEW_CONTACT = `mutation($id: Int!, $cid: Int!, $name: String!, $email:String, $phone: String){
-  Customer_Customer_contactCreate(
+  data: Customer_Customer_contactCreate(
     entity:{
       Customer_id:$id
       Customer_contact_type_id:$cid

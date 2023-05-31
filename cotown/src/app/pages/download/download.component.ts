8f285b850cc87dd15b723b476f7225c32c6a3e5d
Alejandro André <alejandro.andre@ciberexperis.es>
2023-05-31 18:57:02 +0200

    Fix URL

--- cotown/src/app/pages/download/download.component.ts
+++ cotown/src/app/pages/download/download.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { DateAdapter } from '@angular/material/core';
 import { MatDatepicker } from '@angular/material/datepicker';
-
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import {default as _rollupMoment, Moment, unitOfTime} from 'moment';
 
 import { ApolloQueryApi } from 'src/app/services/apollo-api.service';
@@ -20,36 +20,52 @@ const moment = _rollupMoment;
 export class DownloadComponent {
 
   // Dates
-  billDate = new FormControl(moment());
-  paymentDate = new FormControl(moment());
+  billDate = new FormControl();
+  paymentDate = new FormControl();
 
   // Constructor
   constructor(
     private apolloApi: ApolloQueryApi,
     private adapter: DateAdapter<any>,
     private language: LanguageService,
+    private sanitizer:DomSanitizer
   ) { 
     this.adapter.setLocale(this.language.lang.substring(0,2));
   }    
 
   // Link
-  link(data: string) : string {
+  link(data: string) : SafeUrl {
+
+    // Default
+    let l = 'javascript:void(0)';
+
+    // Facturas
     if (data == "facturas") {
-      const from = moment(this.billDate.value);
-      const to = moment(from).add(1, 'M');
-      return environment.backURL + '/export/facturas' 
-        + '?desde=' + from.format('YYYY-MM-DD') 
-        + '&hasta=' + to.format('YYYY-MM-DD') 
-        + '&access_token=' + this.apolloApi.token;
+      if (this.billDate.value) {
+        const from = moment(this.billDate.value);
+        const to = moment(from).add(1, 'M');
+        l = environment.backURL + '/export/facturas' 
+          + '?desde=' + from.format('YYYY-MM-DD') 
+          + '&hasta=' + to.format('YYYY-MM-DD') 
+          + '&access_token=' + this.apolloApi.token;
+      }
+
+    // Pagos
+    } else if (data == "pagos") {
+      if (this.paymentDate.value) {
+        const to = moment(this.paymentDate.value);
+        l =  environment.backURL + '/export/pagos?' 
+          + 'fecha=' + to.format('YYYY-MM-DD') 
+          + '&access_token=' + this.apolloApi.token;
+      }
+
+    // Resto
+    } else {
+      l = environment.backURL + '/export/' + data + '?access_token=' + this.apolloApi.token;
     }
-    if (data == "pagos") {
-      const to = moment(this.paymentDate.value);
-      return environment.backURL + '/export/pagos?' 
-        + 'fecha=' + to.format('YYYY-MM-DD') 
-        + '&access_token=' + this.apolloApi.token;
-    }
-    return environment.backURL + '/export/' + data 
-        + '?access_token=' + this.apolloApi.token;
+
+    // Return
+    return this.sanitizer.bypassSecurityTrustUrl(l);
   }
 
   setMonthAndYear(value: Moment, datepicker: MatDatepicker<Moment>) {

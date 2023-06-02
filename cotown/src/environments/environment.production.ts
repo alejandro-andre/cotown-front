80f54c7bf9b98737a8020f0832999e0065767343
Alejandro André <alejandro.andre@ciberexperis.es>
2023-06-02 13:15:44 +0200

    Production envs

--- cotown/src/environments/environment.production.ts
+++ cotown/src/environments/environment.production.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  backURL: 'https://pre.cotown.ciber.es/api/v1',
+  backURL: 'https://back.cotown.com/api/v1',
   graphqlURL: 'https://core.cotown.com/graphql'
 };
   

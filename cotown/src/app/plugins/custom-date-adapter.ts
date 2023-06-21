6f6ee6dc30d96eaaccf2ca5d415e5c783d74f828
Alejandro André <alejandro.andre@ciberexperis.es>
2023-06-21 13:40:28 +0200

    Fix

--- cotown/src/app/plugins/custom-date-adapter.ts
+++ cotown/src/app/plugins/custom-date-adapter.ts
@@ -1,4 +1,9 @@
+import { Injectable } from '@angular/core';
 import { NativeDateAdapter } from '@angular/material/core';
+
+@Injectable({
+  providedIn: 'root'
+})
 
 export class CustomDateAdapter extends NativeDateAdapter {
   override getFirstDayOfWeek(): number {

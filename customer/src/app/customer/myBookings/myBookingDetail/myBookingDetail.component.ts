75b0da01296c6f8320a2365e90ffb1761b03d028
aandre <alejandro.andre@experis.es>
2024-12-17 16:32:35 +0100

    Fix?

--- customer/src/app/customer/myBookings/myBookingDetail/myBookingDetail.component.ts
+++ customer/src/app/customer/myBookings/myBookingDetail/myBookingDetail.component.ts
@@ -253,21 +253,22 @@ export class MyBookingDetailComponent {
     // Look up checkin options
     const result: any = []
     this.lookupService.checkinOptions.forEach(option => {
-      option.prices.forEach((price: any) =>{
-        if (price.location == this.booking.resource.building.district.location) {
-          let from = price.timetable.Week_from; 
-          let to   = price.timetable.Week_to;
-          if (dow == 0) { from = price.timetable.Sun_from; to = price.timetable.Sun_to; }
-          else if (dow == 6) { from = price.timetable.Sat_from; to = price.timetable.Sat_to; }
-          else if (dow == 5) { from = price.timetable.Fri_from; to = price.timetable.Fri_to; }
-          if (from != undefined) {
-            if ((this.checkintime || '00:00') >= from.substring(0, 5) && (this.checkintime || '99:99') <= this.minusOneMinute(to)) {
-              option.price = price.price;
-              result.push(option);
+      if (option.prices)
+        option.prices.forEach((price: any) =>{
+          if (price.location == this.booking.resource.building.district.location) {
+            let from = price.timetable.Week_from; 
+            let to   = price.timetable.Week_to;
+            if (dow == 0) { from = price.timetable.Sun_from; to = price.timetable.Sun_to; }
+            else if (dow == 6) { from = price.timetable.Sat_from; to = price.timetable.Sat_to; }
+            else if (dow == 5) { from = price.timetable.Fri_from; to = price.timetable.Fri_to; }
+            if (from != undefined) {
+              if ((this.checkintime || '00:00') >= from.substring(0, 5) && (this.checkintime || '99:99') <= this.minusOneMinute(to)) {
+                option.price = price.price;
+                result.push(option);
+              }
             }
           }
-        }
-      })
+        })
     })
 
     // Options
